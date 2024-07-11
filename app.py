9efb53fae7ad592c57446afdbd9665ca4247f27c
Bruno-Cunha-Souza <brunosouza,cup@gmail.com>
2024-07-11 16:14:05 -0300

    create inheritance

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
+bebida_suco = Bebida('Suco de laranja', 7.50 , 'grande')
+prato_pao  = Prato('Pão na chapa', 6.50, 'Pão frances na chapa com manteiga')
 
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
 
 if __name__ == '__main__':
     main()
